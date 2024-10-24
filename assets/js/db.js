4b9f602626930a1080fddd9c40cd64f8aa4619e7
Armand1711 <125480832+Armand1711@users.noreply.github.com>
2024-10-24 10:50:59 +0200

    Update db.js

--- assets/js/db.js
+++ assets/js/db.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 // Ensure your password is URL-encoded if it contains special characters.
-const uri = 'mongodb+srv://231181:Naude123@ecoconnect.af9fo.mongodb.net/EcoConnect?retryWrites=true&w=majority';
+const uri = 'mongodb+srv://231181:<Naude123>@ecoconnect.af9fo.mongodb.net/EcoConnect?retryWrites=true&w=majority';
 
 mongoose.connect(uri)
     .then(() => console.log('MongoDB connected successfully'))
