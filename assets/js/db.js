819d0bf4f51241d79f7f576be6a1e2f9eec96155
Armand1711 <125480832+Armand1711@users.noreply.github.com>
2024-10-24 10:39:27 +0200

    updated mongo conection

--- assets/js/db.js
+++ assets/js/db.js
@@ -1,9 +1,9 @@
-// db.js
 const mongoose = require('mongoose');
 
-const uri = 'mongodb+srv://231181:<Q2nozDvQ6H82tAMI>@ecoconnect.af9fo.mongodb.net/'; 
+// Make sure the password is correct and URL-encoded if it contains special characters.
+const uri = 'mongodb+srv://231181:Q2nozDvQ6H82tAMl@ecoconnect.af9fo.mongodb.net/EcoConnect?retryWrites=true&w=majority';
 
-mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(uri)
     .then(() => console.log('MongoDB connected successfully'))
     .catch(err => console.error('MongoDB connection error:', err));
 
